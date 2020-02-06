498808579b1531d92c786d219efe11262a1fc45b
tbm <tbm98.dev@gmail.com>
2020-02-06 22:14:12 +0700

    apply provider in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:make_sleep_better/src/notifiers.dart';
 import 'package:make_sleep_better/src/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (_) => MainNotifier(),
+      )
+    ], child: MyApp()));
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Dem',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+    return Selector<MainNotifier, bool>(
+      selector: (context, notifier) => notifier.darkMode,
+      builder: (context, data, child) {
+        print('rebuild material app');
+
+        return MaterialApp(
+          title: 'Flutter Dem',
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              brightness: data ? Brightness.dark : Brightness.light),
+          home: child,
+        );
+      },
+      child: HomePage(),
     );
   }
 }
