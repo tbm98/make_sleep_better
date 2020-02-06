0717549eb4598a9761e82c303eae447d335f58de
tbm <tbm98.dev@gmail.com>
2020-02-06 22:15:01 +0700

    extract method to readability

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:make_sleep_better/src/notifiers.dart';
 import 'package:make_sleep_better/src/obj/time_wake_up.dart';
+import 'package:make_sleep_better/src/pages/profile_page.dart';
 import 'package:make_sleep_better/src/supports/dates.dart';
 import 'package:make_sleep_better/src/supports/sizes.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -37,118 +41,150 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Make Sleep Better'),
+          centerTitle: true,
+          leading: _leadingAppbar(),
+          actions: _actionAppbars(),
         ),
-        body: LayoutBuilder(builder: (context, constrains) {
-          return _body(context);
-        }));
+        body: Builder(
+          builder: (context) {
+            return _body(context);
+          },
+        ));
+  }
+
+  Widget _leadingAppbar() {
+    return IconButton(
+      icon: Icon(Icons.brightness_3),
+      onPressed: () {
+        Provider.of<MainNotifier>(context, listen: false)
+            .switchBrightnessMode();
+      },
+    );
+  }
+
+  List<Widget> _actionAppbars() {
+    return [
+      IconButton(
+        icon: Icon(Icons.account_circle),
+        onPressed: () {
+          Navigator.push(
+              context, CupertinoPageRoute(builder: (_) => ProfilePage()));
+        },
+      )
+    ];
   }
 
   Container _body(BuildContext context) {
     return Container(
       child: Column(
         children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RichText(
-                text: TextSpan(
+          _textHeader(context),
+          _timeSetting(context),
+          _listTimeWakeup(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _textHeader(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: RichText(
+          text: TextSpan(
 //                      text: 'I am Minh',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: Sizes.getWidth(context) / 16),
-                    children: [
-                      TextSpan(text: 'Nếu bạn đi ngủ vào '),
-                      TextSpan(text: '8:00 AM'),
-                      TextSpan(
-                          text:
-                              ', bạn nên thức giấc vào những thời điểm sau: '),
-                      TextSpan(text: '11:00 PM'),
-                      TextSpan(text: ' '),
-                      TextSpan(text: ''),
-                      TextSpan(
-                          text:
-                              '(Thức dậy giữa một chu kỳ giấ cngủ khiến bạn cảm thấy mệt mỏi, nhưng khi thức dậy vào lsdlsj')
-                    ]),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: RangeSlider(
-                    values: _rangeValues,
-                    onChanged: (newValue) {
-                      setState(() {
-                        _rangeValues = newValue;
-                        _loadTimeWakeUp();
-                      });
-                    },
-                    min: 1.0,
-                    max: 10.0,
-                    divisions: 9,
-                    labels: RangeLabels(_labelCycle(_rangeValues.start),
-                        _labelCycle(_rangeValues.end)),
-                  ),
-                ),
-                Chip(
-                  avatar: Icon(
-                    Icons.history,
-                    color: Colors.blue,
-                  ),
-                  label: Text(
-                    '8:00',
-                  ),
-                  labelStyle: TextStyle(
-                      color: Colors.blue,
-                      fontSize: Sizes.getWidth(context) / 17),
-                  backgroundColor: Colors.blue[50],
-                  shadowColor: Colors.red,
-                ),
-                IconButton(
-                  icon: Icon(Icons.arrow_forward_ios),
-                  onPressed: () {},
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: Sizes.getHeightNoAppbar(context) * 0.5,
-            child: FutureBuilder(
-              future: _timeWakeUp,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  List<TimeWakeUp> timeWakeUps = snapshot.data;
+              style: TextStyle(
+                  color: Colors.black, fontSize: Sizes.getWidth(context) / 16),
+              children: [
+                TextSpan(text: 'Nếu bạn đi ngủ vào '),
+                TextSpan(text: '8:00 AM'),
+                TextSpan(text: ', bạn nên thức giấc vào những thời điểm sau: '),
+                TextSpan(text: '11:00 PM'),
+                TextSpan(text: ' '),
+                TextSpan(text: ''),
+                TextSpan(
+                    text:
+                        '(Thức dậy giữa một chu kỳ giấ cngủ khiến bạn cảm thấy mệt mỏi, nhưng khi thức dậy vào lsdlsj')
+              ]),
+        ),
+      ),
+    );
+  }
 
-                  return ListView.separated(
-                      itemBuilder: (context, index) {
-                        TimeWakeUp data = timeWakeUps[index];
-
-                        return ListTile(
-                          leading: Text(
-                            _dateSupport.format(data.time) +
-                                data.cycle.toString(),
-                            style: TextStyle(
-                                fontSize: Sizes.getWidth(context) / 16),
-                          ),
-                          trailing:
-                              Switch.adaptive(value: false, onChanged: (_) {}),
-                        );
-                      },
-                      separatorBuilder: (_, __) {
-                        return Divider();
-                      },
-                      itemCount: data.length);
-                } else {
-                  return Center(child: CircularProgressIndicator());
-                }
+  Widget _timeSetting(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: RangeSlider(
+              values: _rangeValues,
+              onChanged: (newValue) {
+                setState(() {
+                  _rangeValues = newValue;
+                  _loadTimeWakeUp();
+                });
               },
+              min: 1.0,
+              max: 10.0,
+              divisions: 9,
+              labels: RangeLabels(_labelCycle(_rangeValues.start),
+                  _labelCycle(_rangeValues.end)),
             ),
+          ),
+          Chip(
+            avatar: Icon(
+              Icons.history,
+              color: Colors.blue,
+            ),
+            label: Text(
+              '8:00',
+            ),
+            labelStyle: TextStyle(
+                color: Colors.blue, fontSize: Sizes.getWidth(context) / 17),
+            backgroundColor: Colors.blue[50],
+            shadowColor: Colors.red,
+          ),
+          IconButton(
+            icon: Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
           )
         ],
+      ),
+    );
+  }
+
+  Widget _listTimeWakeup(BuildContext context) {
+    return SizedBox(
+      height: Sizes.getHeightNoAppbar(context) * 0.5,
+      child: FutureBuilder(
+        future: _timeWakeUp,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<TimeWakeUp> timeWakeUps = snapshot.data;
+
+            return ListView.separated(
+                itemBuilder: (context, index) {
+                  TimeWakeUp data = timeWakeUps[index];
+
+                  return ListTile(
+                    leading: Text(
+                      _dateSupport.format(data.time),
+                      style: TextStyle(fontSize: Sizes.getWidth(context) / 16),
+                    ),
+                    trailing: Switch.adaptive(value: false, onChanged: (_) {}),
+                  );
+                },
+                separatorBuilder: (_, __) {
+                  return Divider();
+                },
+                itemCount: timeWakeUps.length);
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
