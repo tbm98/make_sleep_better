d55bd75e329e182ab4339aafb4458670581a1794
tbm98 <tbm98.dev@gmail.com>
2020-02-22 18:28:08 +0700

    add string & date support

--- lib/src/supports/dates.dart
+++ lib/src/supports/dates.dart
@@ -3,11 +3,11 @@ import 'package:intl/intl.dart';
 import 'package:make_sleep_better/src/obj/time_wake_up.dart';
 
 class DateSupport {
-  DateTime time;
-
   DateSupport() {
     time = DateTime.now();
   }
+
+  DateTime time;
 
   String formatWithoutDay() {
     return DateFormat('HH:mm').format(time);
@@ -18,13 +18,19 @@ class DateSupport {
 //    return DateFormat..format(time);
   }
 
-  Future<List<TimeWakeUp>> getTimesWakeUp(
-      DateTime sleep, int delayMinutes, double minCycle, double maxCyle) async {
+  List<TimeWakeUp> getTimesWakeUp(
+      DateTime sleep, int delayMinutes, double minCycle, double maxCyle) {
     List<TimeWakeUp> times = [];
     for (int i = minCycle.toInt(); i <= maxCyle.toInt(); i++) {
       times.add(
           TimeWakeUp(sleep.add(Duration(minutes: i * 90 + delayMinutes)), i));
     }
     return times;
+  }
+
+  String formatTime(TimeOfDay timeOfDay) {
+    DateTime tmp = DateTime(
+        time.year, time.month, time.day, timeOfDay.hour, timeOfDay.minute);
+    return DateFormat('HH:mm').format(tmp);
   }
 }
--- lib/src/supports/strings.dart
+++ lib/src/supports/strings.dart
@@ -1,15 +1,28 @@
-class Strings{
+class Strings {
   /// store suggest from 1 to 10 cycle sleep
   static const List<String> suggest_cycle = [
     'Too short',
     'Too short',
     'Short, but ok',
-    'Short, but ok',
-    'Short, but ok',
+    'Recommended',
     'Perfect! Recommended',
+    'Recommended',
     'Long',
     'Too long',
     'Too long',
     'Too long'
+  ];
+
+  static const List<String> time_cycle = [
+    '1h30p',
+    '3h',
+    '4h30p',
+    '6h',
+    '7h30p',
+    '9h',
+    '10h30p',
+    '12h',
+    '13h30p',
+    '15h'
   ];
 }
