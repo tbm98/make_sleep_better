4bb8801a1c169cb0e6ca52c3cc18928b37dea1de
tbm98 <tbm98.dev@gmail.com>
2020-02-23 17:13:06 +0700

    add data.id

--- lib/src/obj/data.dart
+++ lib/src/obj/data.dart
@@ -1,14 +1,18 @@
+import 'package:flutter/material.dart';
+
 class Data {
-  Data({this.timeWakeUp, this.feedback, this.level});
+  Data({@required this.id, this.timeWakeUp, this.feedback, this.level});
 
   factory Data.fromMap(Map<String, dynamic> map) {
     return Data(
+      id: map['id'] as int,
       timeWakeUp: DateTime.parse(map['timeWakeUp']),
       feedback: map['feedback'] as bool,
       level: map['level'] as int,
     );
   }
 
+  int id;
   DateTime timeWakeUp;
   bool feedback;
 
@@ -20,6 +24,7 @@ class Data {
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'timeWakeUp': timeWakeUp.toString(),
       'feedback': feedback,
       'level': level,
@@ -28,6 +33,7 @@ class Data {
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'timeWakeUp': timeWakeUp,
       'feedback': feedback,
       'level': level,
