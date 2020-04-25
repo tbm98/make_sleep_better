0db52b4b0a35a35e8cf3d1a4186997f4001d02be
tbm <tbm98.dev@gmail.com>
2020-04-26 00:02:23 +0700

    không ignore main.dart nữa

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'src/supports/logs.dart';
+import 'src/providers/main.dart';
+import 'src/pages/home.dart';
+import 'src/supports/prefs.dart';
+
+void main() => runApp(MultiProvider(providers: [
+      Provider<PrefsSupport>(
+        create: (_) => PrefsSupport(),
+      ),
+      ChangeNotifierProvider(
+        create: (_) => MainProvider(),
+      )
+    ], child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Selector<MainProvider, bool>(
+      selector: (context, notifier) => notifier.darkMode,
+      builder: (context, data, child) {
+        logs('rebuild material app');
+
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Make sleep better - Giúp giấc ngủ tốt hơn',
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              brightness: data ? Brightness.dark : Brightness.light),
+          home: child,
+        );
+      },
+      child: HomePage(),
+    );
+  }
+  // huhu
+  // hlsjdkjsdflj
+}
