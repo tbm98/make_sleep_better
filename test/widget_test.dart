51d212ff6803b7590197115060ef8907f538525a
tbm <tbm98.dev@gmail.com>
2020-05-30 08:05:20 +0700

    update widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,42 +5,52 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:make_sleep_better/src/obj/data.dart';
-import 'package:make_sleep_better/src/supports/logs.dart';
+import 'package:make_sleep_better/main.dart';
+import 'package:make_sleep_better/src/providers/main.dart';
+import 'package:make_sleep_better/src/supports/prefs.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    final Data data =
-        Data(timeWakeUp: DateTime.now(), feedback: true, level: 2);
+//    final Data data =
+//        Data(timeWakeUp: DateTime.now(), feedback: true, level: 2);
 
     //test json convert
-    logs(jsonEncode(data));
+//    logs(jsonEncode(data));
 //    const FileStore fileStore = FileStore();
 //    String value = await fileStore.readData();
 //    print('value is:$value');
-    final List<Data> listData = [data, data];
-    final String result = jsonEncode(listData);
-    logs(result);
+//    final List<Data> listData = [data, data];
+//    final String result = jsonEncode(listData);
+//    logs(result);
 
-    final listDynamic = jsonDecode(result) as List;
-    logs(jsonEncode(
-        listDynamic.map((e) => Data.fromMap(e)).toList()..add(data)));
+//    final listDynamic = jsonDecode(result) as List;
+//    logs(jsonEncode(
+//        listDynamic.map((e) => Data.fromMap(e)).toList()..add(data)));
 
     //test decode empty string
-    try {
-      final a = jsonDecode('') as List;
-      expect(a, null);
-    } catch (e) {
-      expect(e, isNotNull);
-    }
-    try {
-      final a = jsonDecode('[]') as List;
-      expect(a, isNotNull);
-    } catch (e) {
-      expect(e, null);
-    }
+//    try {
+//      final a = jsonDecode('') as List;
+//      expect(a, null);
+//    } catch (e) {
+//      expect(e, isNotNull);
+//    }
+//    try {
+//      final a = jsonDecode('[]') as List;
+//      expect(a, isNotNull);
+//    } catch (e) {
+//      expect(e, null);
+//    }
+
+    await tester.pumpWidget(MultiProvider(providers: [
+      Provider<PrefsSupport>(
+        create: (_) => PrefsSupport(),
+      ),
+      ChangeNotifierProvider(
+        create: (_) => MainProvider(),
+      )
+    ], child: MyApp()));
   });
 }
